af60fc5d5b4ca261c1a54988b7d44cc51d3a1358
JoaoLucasPereiraRamalho <jucasramalho@gmail.com>
2025-11-23 23:46:22 -0300

    refactor: implement responsive stacking for search fields

--- Carrossel.js
+++ Carrossel.js
@@ -1,17 +1,21 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-    nextImage();
-}, 2000)
+setInterval(function () {
+  nextImage();
+}, 2000);
 
-function nextImage(){
-    count++;
-    if(count > 4){
-        count = 1;
-    }
+function nextImage() {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
 
-    document.getElementById("radio"+count).checked = true; 
-
-
+  document.getElementById("radio" + count).checked = true;
 }
+
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  // Esta função adiciona ou remove a classe 'menu-open' na tag <header>.
+  // O CSS que definimos usa essa classe para mostrar ou esconder o menu mobile.
+  document.querySelector(".header").classList.toggle("menu-open");
+});
